f5068ab7b8daf9d7125568f5887b7f222e4d6320
bberenice02 <131243905+bberenice02@users.noreply.github.com>
2024-10-11 13:35:04 +0200

    services first commit

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -6,7 +6,7 @@ import Backend from './Backend';
 const Skills = () => {
     return (
         <section className="skills section" id="skills">
-            <h2 className="section__title">My Skills</h2>
+            <h2 className="section__title">Skills</h2>
             <span className="section__subtitle">My technical level</span>
 
             <div className="skills__container container grid">
