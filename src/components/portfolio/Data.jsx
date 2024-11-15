437e104cfee1a3b63d620b9d5299209159c20271
bberenice02 <131243905+bberenice02@users.noreply.github.com>
2024-11-15 19:29:25 +0100

    commit avant tests mise en page

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,8 +1,9 @@
 import Project1 from "../../assets/hypercube.jpg";
-import Project2 from "../../assets/ARapp.png";
-import Project3 from "../../assets/work3.jpg";
+import Project2 from "../../assets/work2.jpg";
+import Project3 from "../../assets/ARapp.png";
 import Project4 from "../../assets/work4.jpg";
 import Project5 from "../../assets/work5.jpg";
+import Project6 from "../../assets/work1.jpg";
 
 
 export const Data = [
@@ -10,30 +11,72 @@ export const Data = [
       id: 1,
       image: Project1,
       title: "Hypercube à miroirs infini",
-      description: "web",
+      context :"Projet de 4ème année, réalisation d’un hypercube à miroirs infini pour exposer durant les portes ouvertes de l’école, matériel pédagogique pour l’apprentissage d’arduino.",
+      duration :"de septembre 2023 à mai 2024 (8-9 mois)",
+      technologies :"Solidworks, Arduino",
+      description: "Réalisation complète du début à la fin d’un hypercube à miroirs infini, choix des matériaux adaptés selon les contraintes de budget, de matériel, confection de pièces 3D pour positionner correctement le ruban de LED à l'intérieur. Soudure sur le ruban de LED.",
+      role :"Projet de 2 personnes, conception de modèle et de pièces 3D, soudure, montage.",
+      results :"Cube fonctionnel avec le résultat attendu.",
+      skills :"Compétence de conception manuelle, de réflexion pour les choix de composants, matériaux, en fonction de contraintes. Recherche de solutions pour répondre aux contraintes.",
     },
     {
       id: 2,
       image: Project2,
-      title: "Application mobile en Réalité augmentée",
-      description: "app",
+      title: "Cap Ingénieuses",
+      context :"Projet de 3ème année",
+      duration :"de janvier à juin 2023 (6 mois)",
+      technologies :"Trello pour la gestion de projet",
+      description: "Réalisation de 6 expériences scientifiques adaptées à des élèves de primaire pour leur enseigner les sciences et plus particulièrement les femmes ingénieurs dans la sciences et la possibilité pour les jeunes filles de s’engager vers cette voie.",
+      role :"Équipe de 6 personnes, cheffe de projet, coordination des membres, répartition des tâches.",
+      results :"Des élèves de primaire satisfait de notre intervention",
+      skills :"Capacité de rendre des phénomènes physiques, scientifiques, compréhensible par des enfants de 10 ans. Adaptabilité, choix d’expériences adaptées",
     },
     {
       id: 3,
       image: Project3,
-      title: "Brand design",
-      description: "design",
+      title: "Application en Réalité Augmentée",
+      context :"Dans le cadre d’un cours de réalité augmentée réalisation d’une application android pouvant être utilisée dans la vie de tous les jours",
+      duration :"quelques semaines (avril-mai 2024)",
+      technologies :" Unity, C#",
+      description: "Mon application de réalité augmentée permet en positionnant devant la caméra de votre smartphone, des cartes d’instruments de musiques et de voir apparaître le modèle 3D des instruments ainsi que d’entendre un extrait musical de l’instrument présenté",
+      role :"Projet réalisé individuellement",
+      results :"Une application fonctionnelle qui fait apparaître et jouer l’instrument dès qu’il apparaît à l’écran.",
+      skills :"Autonomie dans la réalisation de ce projet, utilisation de l’application Unity.",
     },
     {
       id: 4,
       image: Project4,
-      title: "App movil",
-      description: "app",
+      title: "Application Android studio",
+      context :"Réalisation d’une application Android grâce à android studio.",
+      duration :"Mai 2024 (2 semaines)",
+      technologies :"Android studio, utilisation d’une API , controller view, Kotlin.",
+      description: " Application utilisant une API de livres, possibilité de rechercher des livres qui s’affichent dans un controller view, possibilité d’ajouter les livres à une liste de favoris.",
+      role :"Projet réalisé individuellement",
+      results :"Application fonctionnelle simple",
+      skills :"utilisation d’android studio, réalisation d’une application fonctionnelle en peu de temps.",
     },
     {
       id: 5,
       image: Project5,
-      title: "Web design",
-      description: "web",
+      title: "Jeu VR Unreal Engine",
+      context :"Réalisation d’un jeux vidéo sur unreal engine dans le cadre du cours “Réalité étendue et interactions multimodales“",
+      duration :"octobre-novembre 2024 (6 semaines)",
+      technologies :"Unreal engine, blueprint",
+      description: "Jeu vidéo de tir/labyrinthe comprenant des éléments vu en TP (porte coulissante, ascenseur, ennemi, bouton switch, User interface.",
+      role :"Projet réalisé individuellement",
+      results :"Cube fonctionnel avec le résultat attendu.",
+      skills :"Utilisation d’unreal engine, design d’un jeu fonctionnel",
+    },
+    {
+      id: 6,
+      image: Project6,
+      title: "Projet Linkterz",
+      context :"Dans le cadre du cours “Design d’interface utilisateur” réaliser un design d’application sur figma tout en respectant la charte graphique.",
+      duration :"2024 quelques semaines/mois",
+      technologies :"Figma",
+      description: "Réalisation du design d’une application contrôlant les objets connectés d’une maison sur Figma.",
+      role :"Équipe de 3 personnes",
+      results :"",
+      skills :"Utilisation de figma",
     },
   ];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,13 +9,26 @@ const Portfolio = () => {
             <span className="section__subtitle">My projects</span>
 
             <div className="portfolio__container">
-            {Data.map(({id, image, title, description }) => {
+            {Data.map(({id, image, title, context, duration, technologies, description, role, results, skills }) => {
                 return (
                     <div className="portfolio__card" key={id}>
                         <img src={image} alt="" className="portfolio__img" />
 
                         <h3 className="portfolio__name">{title}</h3>
+                        <div className="portfolio__subtitle">Contexte: </div>
+                        <p className="portfolio__context">{context}</p>
+                        <div className="portfolio__subtitle">Durée et période: </div>
+                        <p className="portfolio__duration">{duration}</p>
+                        <div className="portfolio__subtitle">Technologies utilisées: </div>
+                        <p className="portfolio__technologies">{technologies}</p>
+                        <div className="portfolio__subtitle">Description: </div>
                         <p className="portfolio__description">{description}</p>
+                        <div className="portfolio__subtitle">Rôle: </div>
+                        <p className="portfolio__role">{role}</p>
+                        <div className="portfolio__subtitle">Résultats obtenus: </div>
+                        <p className="portfolio__results">{results}</p>
+                        <div className="portfolio__subtitle">Compétences aquises: </div>
+                        <p className="portfolio__skills">{skills}</p>
                     </div>
                 );
             })}
