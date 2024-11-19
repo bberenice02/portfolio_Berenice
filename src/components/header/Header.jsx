9ffecaf964373a9ea0fd42c770ebf37c1eec9507
bberenice02 <131243905+bberenice02@users.noreply.github.com>
2024-11-19 18:50:26 +0100

    ModifCV commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -49,13 +49,13 @@ const Header = () => {
                                 <i className="uil uil-scenery nav__icon"></i> Portfolio
                             </a>
                         </li>
-                        <li className="nav__item">
+                        {/*<li className="nav__item">
                             <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? "nav__link active-link" : "nav__link"
                                 }
                             >
                                 <i className="uil uil-message nav__icon"></i> Contact
                             </a>
-                        </li>
+                        </li>*/}
                     </ul>
                     <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
